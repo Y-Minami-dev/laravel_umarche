76360cc0ed395421d013628efdaf3d38bebd6256
Yukinori Minami <87700925+Y-Minami-dev@users.noreply.github.com>
2022-01-27 01:23:10 +0900

    middleware, abort(404)

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -34,7 +34,7 @@
                             <td class="md:px-4 py-3">{{ $owner->email }}</td>
                             <td class="md:px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
                             <td class="md:px-4 py-3">
-                              <button onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id ])}}" class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded">編集</button>
+                              <button onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id ])}}'" class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded">編集</button>
                             </td>
                             <form id="delete_{{$owner->id}}" method="post" action="{{route('admin.owners.destroy', ['owner' => $owner->id])}}">
                               @csrf
