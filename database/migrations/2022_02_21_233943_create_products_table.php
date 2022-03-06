eff0f3a6ee3463a20fcc7c73f8d41594641335d7
Yukinori Minami <87700925+Y-Minami-dev@users.noreply.github.com>
2022-03-07 01:16:24 +0900

    product create

--- database/migrations/2022_02_21_233943_create_products_table.php
+++ database/migrations/2022_02_21_233943_create_products_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PhpParser\PrettyPrinterAbstract;
 
 class CreateProductsTable extends Migration
 {
@@ -15,6 +17,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')->nullable();
             $table->foreignId('shop_id')
             ->constrained()
             ->onUpdate('cascade')
@@ -22,6 +29,15 @@ class CreateProductsTable extends Migration
             $table->foreignId('secondary_category_id')
             ->constrained();
             $table->foreignId('image1')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image2')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image3')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image4')
             ->nullable()
             ->constrained('images');
             $table->timestamps();
