a4891c05107514449c799aae6a20bdf7652aaa3a
Yukinori Minami <87700925+Y-Minami-dev@users.noreply.github.com>
2022-03-20 18:54:32 +0900

    cart index

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -84,7 +84,7 @@
                                 </div>
                                 <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
                                 <input type="hidden" name="product_id" value="{{ $product->id}}">    
-                            </form>>
+                            </form>
                             </div>
                         </div>
                     </div>
