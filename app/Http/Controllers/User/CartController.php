95eec3869cd5fd80991eb7a7d02b59325c39ff55
Yukinori Minami <87700925+Y-Minami-dev@users.noreply.github.com>
2022-03-21 22:08:10 +0900

    stripe決済テスト

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -87,8 +87,6 @@ class CartController extends Controller
             ]);
         }
 
-        dd('test');
-
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 
         $session = \Stripe\Checkout\Session::create([
@@ -96,7 +94,7 @@ class CartController extends Controller
             'line_items' => [$lineItems],
             'mode' => 'payment',
             'success_url' => route('user.items.index'),
-            'cancel_url' => route('cart.cart.index'),
+            'cancel_url' => route('user.cart.index'),
             ]);
 
             $publicKey = env('STRIPE_PUBLIC_KEY');
