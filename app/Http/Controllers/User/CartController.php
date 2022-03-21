8c60d5a563928ef315eb069b99480caa5ef9c4f2
Yukinori Minami <87700925+Y-Minami-dev@users.noreply.github.com>
2022-03-22 00:57:19 +0900

    決済後のカート内商品削除

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -9,22 +9,21 @@ use App\Models\User;
 use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 
-
 class CartController extends Controller
 {
     public function index()
     {
         $user = User::findOrFail(Auth::id());
-        $products = $user->products; // 多対多のリレーション
+        $products = $user->products;
         $totalPrice = 0;
 
         foreach($products as $product){
             $totalPrice += $product->price * $product->pivot->quantity;
-         }
+        }
 
-         //dd($products, $totalPrice);
+        // dd($products, $totalPrice);
 
-        return view('user.cart',
+        return view('user.cart', 
             compact('products', 'totalPrice'));
     }
 
@@ -34,22 +33,26 @@ class CartController extends Controller
         ->where('user_id', Auth::id())->first();
 
         if($itemInCart){
-            $itemInCart->quantity += $request->quantity; //あれば数量を追加
+            $itemInCart->quantity += $request->quantity;
             $itemInCart->save();
+
         } else {
-            Cart::create([ // なければ新規作成
+            Cart::create([
                 'user_id' => Auth::id(),
                 'product_id' => $request->product_id,
                 'quantity' => $request->quantity
             ]);
         }
+        
         return redirect()->route('user.cart.index');
     }
 
     public function delete($id)
     {
         Cart::where('product_id', $id)
-        ->where('user_id', Auth::id())->delete();
+        ->where('user_id', Auth::id())
+        ->delete();
+
         return redirect()->route('user.cart.index');
     }
 
@@ -57,33 +60,31 @@ class CartController extends Controller
     {
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
-
+        
         $lineItems = [];
         foreach($products as $product){
             $quantity = '';
             $quantity = Stock::where('product_id', $product->id)->sum('quantity');
 
-            if($product->pivot->quantity > $quantity ){
+            if($product->pivot->quantity > $quantity){
                 return redirect()->route('user.cart.index');
-                } else {
-                    $lineItem = [
-                        'name' => $product->name,
-                        'description' => $product->information,
-                        'amount' => $product->price,
-                        'currency' => 'jpy',
-                        'quantity' => $product->pivot->quantity,
-                    ];
-                    array_push($lineItems, $lineItem);
-                }
-
-            
+            } else {
+                $lineItem = [
+                    'name' => $product->name,
+                    'description' => $product->information,
+                    'amount' => $product->price,
+                    'currency' => 'jpy',
+                    'quantity' => $product->pivot->quantity,
+                ];
+                array_push($lineItems, $lineItem);    
+            }
         }
-        //dd($lineItems);
+        // dd($lineItems);
         foreach($products as $product){
             Stock::create([
-            'product_id' => $product->id,
-            'type' => \Constant::PRODUCT_LIST['reduce'],
-            'quantity' => $product->pivot->quantity * -1
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1
             ]);
         }
 
@@ -93,13 +94,35 @@ class CartController extends Controller
             'payment_method_types' => ['card'],
             'line_items' => [$lineItems],
             'mode' => 'payment',
-            'success_url' => route('user.items.index'),
-            'cancel_url' => route('user.cart.index'),
+            'success_url' => route('user.cart.success'),
+            'cancel_url' => route('user.cart.cancel'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout', 
+            compact('session', 'publicKey'));
+    }
+
+    public function success()
+    {
+        Cart::where('user_id', Auth::id())->delete();
+
+        return redirect()->route('user.items.index');
+    }
+
+    public function cancel()
+    {
+        $user = User::findOrFail(Auth::id());
+
+        foreach($user->products as $product){
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['add'],
+                'quantity' => $product->pivot->quantity
             ]);
+        }
 
-            $publicKey = env('STRIPE_PUBLIC_KEY');
-
-            return view('user.checkout',
-                compact('session', 'publicKey'));
+        return redirect()->route('user.cart.index');
     }
 }
