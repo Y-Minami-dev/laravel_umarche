dac06d2b4982ecb9fb34ab9add1dbdf16c014344
Yukinori Minami <87700925+Y-Minami-dev@users.noreply.github.com>
2022-03-31 01:01:44 +0900

    ユーザー向けメール送信

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -17,8 +17,12 @@ class CartService
             $values = array_values($owner); //連想配列の値を取得
             $keys = ['ownerName', 'email'];
             $ownerInfo = array_combine($keys, $values); // オーナー情報のキーを変更
-
-            dd($ownerInfo);
+            $product = Product::where('id', $item->product_id)
+            ->select('id', 'name', 'price')->get()->toArray(); // 商品情報の配列
+            $quantity = Cart::where('product_id', $item->product_id)
+            ->select('quantity')->get()->toArray(); // 在庫数の配列
+            $result = array_merge($product[0], $ownerInfo, $quantity[0]); // 配列の結合
+            array_push($products, $result); //配列に追加
         }
         return $products; // 新しい配列を返す
     }
